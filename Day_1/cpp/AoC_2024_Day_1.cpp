09b13311c3fcc131697d7d3bfdff8efcab7b0881
Joey Sachtleben <manjoey7@gmail.com>
2024-12-02 14:23:16 -0600

    Add code for first two days
    Added existing code from previous individual repos

---
+++ Day_1/cpp/AoC_2024_Day_1.cpp
@@ -0,0 +1,177 @@
+﻿/* AoC_2024_Day_1
+ * Joey Sachtleben
+ * Licenced under the MIT licence. See LICENSE.txt for details.
+ *
+ ***
+ *
+ */
+
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <numeric>
+
+#include "AoC_2024_Day_1.h"
+
+
+using std::cout;
+using std::cerr;
+using std::vector;
+
+using ss = std::stringstream;
+
+namespace fs = std::filesystem;
+
+
+/* ValidatePath
+ *   Creates a path to a specified file and validates that it is a reachable regular file.
+ *
+ * Parameters:
+ *   pathName - A string containing the path to the desired file.
+ *
+ * Returns:
+ *   A path to the file.
+ *
+ * Throws:
+ *   invalid_argument if the file does not exist or is not a regular file.
+ */
+fs::path ValidatePath(const std::string_view pathName)
+{
+	fs::path path = pathName;
+	path.make_preferred();
+
+	if (!fs::exists(path))
+	{
+		ss msg;
+		msg << "File \"" << path.string() << "\" does not exist.\n";
+		throw std::invalid_argument(msg.str());
+	}
+
+	if (!fs::is_regular_file(path))
+	{
+		ss msg;
+		msg << "File \"" << path.string() << "\" is not a valid regular file.\n";
+		throw std::invalid_argument(msg.str());
+	}
+
+	return path;
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc > 2)
+	{
+		cerr << "Too many arguments.\n"
+			 << "Expected usage: " << argv[0] << " [INPUT FILE]\n";
+		return 1;
+	}
+
+	int retval = 0;
+
+	std::string inputFilePath = "../input.txt";
+	if (argc == 2)
+	{
+		inputFilePath = std::string(argv[1]);
+	}
+	cout << "Using file path " << inputFilePath << " ...\n";
+
+	try
+	{
+		fs::path filePath = ValidatePath(inputFilePath);
+
+		std::ifstream input(filePath);
+
+		if(!input.is_open())
+		{
+			ss msg;
+			msg << "Unable to open " << inputFilePath << ".\n";
+			throw std::runtime_error(msg.str());
+		}
+
+		vector<int32_t> list1;
+		vector<int32_t> list2;
+
+		// Read input into two lists
+		int32_t lineCounter = 0;
+		while (input.good())
+		{
+			int32_t n1 = -1;
+			int32_t n2 = -1;
+			input >> n1 >> n2;
+			lineCounter++;
+
+			if (n1 == -1 || n2 == -1)
+			{
+				ss msg;
+				msg << "Error reading numbers from line " << lineCounter << ". Please check the input file.\n";
+				throw std::runtime_error(msg.str());
+			}
+
+			list1.push_back(n1);
+			list2.push_back(n2);
+		}
+
+		// Sort the two lists for pairing
+		std::ranges::sort(list1);
+		std::ranges::sort(list2);
+
+
+		// Part 1 
+		// Find the distance between each pair
+		vector<int32_t> distances;
+		for (size_t i = 0; i < list1.size(); ++i)
+		{
+			distances.push_back(abs(list1.at(i) - list2.at(i)));
+		}
+
+		// Sum the distances
+		auto totalDistance = std::accumulate(distances.begin(), distances.end(), 0);
+
+
+		cout << "The total distance is: " << totalDistance << "!\n";
+
+		// Part 2
+		// Find similarity scores
+		vector<int32_t> oneInTwo;
+		for (int32_t num : list1)
+		{
+			int total = 0;
+			for (int32_t n2 : list2)
+			{
+				if (num == n2)
+				{
+					total++;
+				}
+			}
+			oneInTwo.push_back(total);
+		}
+		// Multiply & sum score
+		int32_t totalScore = 0;
+		for (size_t i = 0; i < list1.size(); ++i)
+		{
+			totalScore += list1.at(i) * oneInTwo.at(i);
+		}
+		// Sum the scores
+
+		cout << "The total similarity score is: " << totalScore << "!\n";
+
+	}
+	catch (std::exception& e)
+	{
+		cerr << "Caught exception: " << e.what() << "\n";
+		retval = 1;
+	}
+	catch (...)
+	{
+		cerr << "Caught unknown exception!\n";
+		retval = 1;
+	}
+
+
+	return retval;
+}
---
+++ Day_2/cpp/AoC_2024_Day_2.cpp
@@ -0,0 +1,239 @@
+﻿/* AoC_2024_Day_2
+ * Joey Sachtleben
+ * Licenced under the MIT licence. See LICENSE.TXT for details.
+ *
+ */
+
+#include <filesystem>
+#include <string>
+#include <sstream>
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "AoC_2024_Day_2.h"
+
+using std::cout;
+using std::cerr;
+using std::vector;
+
+using ss = std::stringstream;
+using Report = std::vector<int>;
+
+namespace fs = std::filesystem;
+
+/* ValidatePath
+ *   Creates a path to a specified file and validates that it is a reachable regular file.
+ *
+ * Parameters:
+ *   pathName - A string containing the path to the desired file.
+ *
+ * Returns:
+ *   A path to the file.
+ *
+ * Throws:
+ *   invalid_argument if the file does not exist or is not a regular file.
+ */
+fs::path ValidatePath(const std::string_view pathName)
+{
+	fs::path path = pathName;
+	path.make_preferred();
+
+	if (!fs::exists(path))
+	{
+		ss msg;
+		msg << "File \"" << path.string() << "\" does not exist.\n";
+		throw std::invalid_argument(msg.str());
+	}
+
+	if (!fs::is_regular_file(path))
+	{
+		ss msg;
+		msg << "File \"" << path.string() << "\" is not a valid regular file.\n";
+		throw std::invalid_argument(msg.str());
+	}
+
+	return path;
+}
+
+bool IsPositive(int i)
+{
+	return abs(i) == i;
+}
+
+/* CheckReportSafety
+ *	Determines if a report meets the requirements to be considered safe.
+ *
+ * Parameters:
+ *	report - The report to be checked.
+ *
+ * Returns:
+ *	A boolean representing if the report was safe.
+ *
+ * Throws:
+ *	May throw exceptions from the Report.
+ */
+bool CheckReportSafety(const Report& report)
+{
+
+	int delta1 = report.at(1) - report.at(0);
+	if (abs(delta1) > 3 || delta1 == 0)
+	{
+		return false;
+	}
+
+	for (size_t i = 2; i < report.size(); ++i)
+	{
+		int delta2 = report.at(i) - report.at(i-1);
+		if (abs(delta2) > 3 || delta2 == 0)
+		{
+			return false;
+		}
+
+		if (IsPositive(delta1) != IsPositive(delta2))
+		{
+			return false;
+		}
+
+		delta1 = delta2;
+
+	}
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc > 2)
+	{
+		cerr << "Too many arguments.\n"
+			<< "Expected usage: " << argv[0] << " [INPUT FILE]\n";
+		return 1;
+	}
+
+	int retval = 0;
+
+	std::string inputFilePath = "../input.txt";
+	if (argc == 2)
+	{
+		inputFilePath = std::string(argv[1]);
+	}
+	cout << "Using file path " << inputFilePath << " ...\n";
+
+	try
+	{
+		fs::path inputPath = ValidatePath(inputFilePath);
+		std::ifstream input(inputPath);
+
+		if (!input.is_open())
+		{
+			ss msg;
+			msg << "Unable to open file " << inputFilePath << " .\n";
+			throw std::runtime_error(msg.str());
+		}
+
+		// Read in all reports
+		vector<Report> reports;
+		int lineCounter = 0;
+		while (input.good())
+		{
+			Report report;
+			std::string rawLine;
+			lineCounter++;
+			std::getline(input, rawLine);
+			if (input.bad())
+			{
+				ss msg;
+				msg << "Error reading line " << lineCounter << " from input file.\n";
+				throw std::runtime_error(msg.str());
+			}
+			
+
+			ss line;
+			line << rawLine;
+			while (!line.eof())
+			{
+				int n = -1;
+				line >> n;
+				if (n == -1)
+				{
+					ss msg;
+					msg << "Error reading next number from the line \"" << line.str() << "\" on line " << lineCounter << ".\n";
+					throw std::runtime_error(msg.str());
+				}
+				report.push_back(n);
+			}
+			reports.emplace_back(report);
+		}
+
+
+		// Find all safe reports
+		vector<bool> isReportSafe;
+		vector<bool> isReportSafeWithDampener;
+		for (const Report& report : reports)
+		{
+			bool isSafe = CheckReportSafety(report);
+			isReportSafe.push_back(isSafe);
+
+			// Do we need to try The Problem Dampener?
+			if (!isSafe)
+			{
+				bool becameSafe = false;
+				for (size_t i = 0; i < report.size(); ++i)
+				{
+					Report modReport = report;
+					modReport.erase(modReport.begin() + i);
+
+					// Is the report now safe?
+					bool isNowSafe = CheckReportSafety(modReport);
+					if (isNowSafe)
+					{
+						becameSafe = true;
+						break;
+					}
+				}
+				isReportSafeWithDampener.push_back(becameSafe);
+			}
+			else
+			{
+				isReportSafeWithDampener.push_back(isSafe);
+			}
+		}
+
+		// Count how many reports were safe
+		int totalSafe = 0;
+		for (bool reportSafe : isReportSafe)
+		{
+			if (reportSafe == true)
+			{
+				totalSafe++;
+			}
+		}
+		int totalSafeWithDampener = 0;
+		for (bool reportSafe : isReportSafeWithDampener)
+		{
+			if (reportSafe == true)
+			{
+				totalSafeWithDampener++;
+			}
+		}
+
+		cout << "There are " << totalSafe << " safe reports out of " << isReportSafe.size() << " reports.\n";
+		cout << "With the problem dampener, there are a total of " << totalSafeWithDampener << " safe reports.\n";
+
+	}
+	catch (std::exception& e)
+	{
+		cerr << "Caught exception: " << e.what() << "\n";
+		retval = 1;
+	}
+	catch (...)
+	{
+		cerr << "Caught unknown exception!\n";
+		retval = 1;
+	}
+
+
+	return retval;
+}
